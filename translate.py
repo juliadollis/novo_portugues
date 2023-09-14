9940a646dfadc5734a6bb2da2868917cbcd8f252
juliadollis <109148571+juliadollis@users.noreply.github.com>
2023-09-14 14:57:00 -0300

    Create translate.py

---
+++ translate.py
@@ -0,0 +1,47 @@
+import os
+import pandas as pd
+import openai
+import time
+import argparse
+
+
+def translate_to_modern_portuguese(text, api_key):
+    print(text)
+    print("Traduzindo...")
+    openai.api_key = api_key
+    prompt = [{"role": "user",
+               "content": f"corrija a grafia da seguinte frase do portugues antigo para o portugues atual, retorne apenas a frase corrigida: {text}"}]
+    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=prompt, temperature=0.2, max_tokens=100)
+    texto_traduzido = response['choices'][0]['message']['content'].strip()
+    print(texto_traduzido)
+    time.sleep(21)
+    return texto_traduzido
+
+
+def batch_translate(df, api_key):
+    translated_texts = []
+    for index, row in df.iterrows():
+        text = row['transcript']
+        translated_texts.append(translate_to_modern_portuguese(text, api_key))
+    return translated_texts
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Tradução de texto')
+    parser.add_argument('--input', required=True, help='Nome do arquivo Excel de entrada')
+    parser.add_argument('--sheet', required=True, help='Nome da planilha do arquivo Excel')
+    parser.add_argument('--output', required=True, help='Nome do arquivo Excel de saída')
+    parser.add_argument('--api_key', required=True, help='Chave da API da OpenAI')
+
+    args = parser.parse_args()
+
+    df = pd.read_excel(args.input, engine='openpyxl', sheet_name=args.sheet)
+
+    if df.empty:
+        print("O DataFrame está vazio. Nada para traduzir.")
+    else:
+        df.dropna(subset=['transcript'], inplace=True)
+        translated_texts = batch_translate(df, args.api_key)
+        df['transcript'] = translated_texts
+        df.to_excel(args.output, engine='openpyxl', index=False)
+        print("O arquivo Excel foi salvo com sucesso!")
