3321c2e4c000ec749bb39421ff81e8bb21c8e6da
juliadollis <109148571+juliadollis@users.noreply.github.com>
2023-09-17 17:42:36 -0300

    Update translate.py

--- translate.py
+++ translate.py
@@ -9,21 +9,21 @@ def translate_to_modern_portuguese(text, api_key):
     print(text)
     print("Traduzindo...")
     openai.api_key = api_key
-     prompt = [{"role": "user",
+    prompt = [{"role": "user",
                "content": f"corrija a grafia da seguinte frase do portugues antigo para o portugues atual, corrigindo "
                           f"numerais e numeros por numeros em extenso e corrigindo a acentuação,  retorne apenas a "
                           f"frase corrigida: {text}"}]
-    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=prompt, temperature=0.2, max_tokens=100)
+    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=prompt, temperature=0.2, max_tokens=200)
     texto_traduzido = response['choices'][0]['message']['content'].strip()
     print(texto_traduzido)
     time.sleep(21)
     return texto_traduzido
 
 
-def batch_translate(df, api_key):
+def batch_translate(df, column_name, api_key):
     translated_texts = []
     for index, row in df.iterrows():
-        text = row['transcript']
+        text = row[column_name]
         translated_texts.append(translate_to_modern_portuguese(text, api_key))
     return translated_texts
 
@@ -32,6 +32,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Tradução de texto')
     parser.add_argument('--input', required=True, help='Nome do arquivo Excel de entrada')
     parser.add_argument('--sheet', required=True, help='Nome da planilha do arquivo Excel')
+    parser.add_argument('--column', required=True, help='Nome da coluna contendo os textos para traduzir')
     parser.add_argument('--output', required=True, help='Nome do arquivo Excel de saída')
     parser.add_argument('--api_key', required=True, help='Chave da API da OpenAI')
 
@@ -42,8 +43,8 @@ if __name__ == "__main__":
     if df.empty:
         print("O DataFrame está vazio. Nada para traduzir.")
     else:
-        df.dropna(subset=['transcript'], inplace=True)
-        translated_texts = batch_translate(df, args.api_key)
-        df['transcript'] = translated_texts
+        df.dropna(subset=[args.column], inplace=True)
+        translated_texts = batch_translate(df, args.column, args.api_key)
+        df[args.column] = translated_texts
         df.to_excel(args.output, engine='openpyxl', index=False)
         print("O arquivo Excel foi salvo com sucesso!")
